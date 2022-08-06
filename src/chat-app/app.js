d02ffc7ac8a09c7b28fcd0e607bfbda5e949470b
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2022-08-06 20:52:28 +0300

    Update

--- src/chat-app/app.js
+++ src/chat-app/app.js
@@ -1,16 +1,13 @@
 
-    import {createApp} from 'https://unpkg.com/vue@3.2.33/dist/vue.esm-browser.prod.js';
-    
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+    import {createApp} from  "https://unpkg.com/vue@3.2.37/dist/vue.esm-browser.js"; 
 
-    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+
+    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
    
+    // Insert Keys Here
 
-
-    //Insert Firestore Keys Here
-
-    
-   
     const app = initializeApp(firebaseConfig);
 
     const db = getFirestore(app);
+
--- src/demo-template/assets/js/app.js
+++ src/demo-template/assets/js/app.js
@@ -1,12 +1,10 @@
 
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
 
-    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js";
    
-    
-    //Insert Firestore Keys Here
+    // Insert Keys Here
 
-   
     const app = initializeApp(firebaseConfig);
 
     const db = getFirestore(app);
